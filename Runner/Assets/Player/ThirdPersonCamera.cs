6c1b66daddb3c4dc03e160ce05ed855f27bdfd23
Raaid - RMIT <s3719919@student.rmit.edu.au>
2024-09-14 03:30:56 +1000

    Fixed rotations

--- Runner/Assets/Player/ThirdPersonCamera.cs
+++ Runner/Assets/Player/ThirdPersonCamera.cs
@@ -9,20 +9,27 @@ public class ThirdPersonCamera : MonoBehaviour
     public Transform player;
     public Transform playerObj;
     public Rigidbody rb;
-
     public float rotationSpeed;
 
-    
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     void Update()
     {
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         if (inputDir != Vector3.zero)
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        {
+            Quaternion toRotation = Quaternion.LookRotation(inputDir, Vector3.up);
+            playerObj.rotation = Quaternion.Lerp(playerObj.rotation, toRotation, Time.deltaTime * rotationSpeed);
+        }
     }
 }
