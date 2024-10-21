1499ceae022acea4a82171aa083be1c8abf0e5e3
Raaid - RMIT <s3719919@student.rmit.edu.au>
2024-10-21 16:29:12 +1100

    Fixed lighting issue in build, changed outlines to only be around NPC and Player due to bug

--- Runner/Assets/UI/StartMenu/Scripts/StartScreen.cs
+++ Runner/Assets/UI/StartMenu/Scripts/StartScreen.cs
@@ -19,3 +19,44 @@ public class StartScreen : MonoBehaviour
         }
     }
 }
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class FadeSequence : MonoBehaviour
+{
+    public RawImage fadeImage;
+    public float duration;
+    public TextMeshProUGUI uiText;
+    public string message;
+
+    public void StartFade()
+    {
+        StartCoroutine(nameof(FadeIn));
+    }
+
+    IEnumerator FadeIn()
+    {
+        Color imageColor = fadeImage.color;
+
+        imageColor.a = 0f;
+        fadeImage.color = imageColor;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            imageColor.a = Mathf.Clamp01(elapsedTime / duration);
+            fadeImage.color = imageColor;
+            yield return null;
+        }
+
+        imageColor.a = 1f;
+        fadeImage.color = imageColor;
+    }
+}
+*/
